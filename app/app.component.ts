a736bc9c4f1c91c4fa7776d468f5339e7a437362
SalmaKolkari <salmakolkari@outlook.com>
2016-12-29 05:39:49 -0500

    new layout file and its dependent js and css files added.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'layout',
+  templateUrl: 'app/layout.html'
 })
 export class AppComponent  { name = 'Angular'; }
